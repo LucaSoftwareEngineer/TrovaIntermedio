86d9674ee3814cd4caf833ba1db042c1285f3ba2
LucaSoftwareEngineer <lucapc40@gmail.com>
2024-09-27 17:58:39 +0200

    first commit

---
+++ src/test/java/com/prog/trovaIntermedio/TrovaIntermedioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog.trovaIntermedio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrovaIntermedioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
